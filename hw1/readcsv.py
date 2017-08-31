25f3bd13bcd27c9181c793029dc8274f922a6f4a
Ken (Huy) Tu <huyqtu7@gmail.com>
2017-08-31 10:46:51 -0400

    Update readcsv.py

--- hw1/readcsv.py
+++ hw1/readcsv.py
@@ -1,7 +1,4 @@
-import sys
 import time
-import os 
-
 
 class readCSV():
     def __init__(self):
@@ -10,7 +7,6 @@ class readCSV():
         self.noc = -1
         self.header_f = False
         self.ignore_cols = []
-        self.runable = True
 
     def format(val):
         try:
@@ -22,7 +18,7 @@ class readCSV():
         except ValueError:
             return val
 
-    def clean(self, line):
+    def clean(self, line, l_index):
         if "#" in line:
             line = line.split("#")[0]
         line = line.strip('\n')
@@ -35,42 +31,48 @@ class readCSV():
                 #print entry
                 if entry:
                     self.header.append(entry)
+                else:
+                    print "There are null element in the header line"
         else:
             if self.noc == len(entries):
+                row = []
                 for index in range(len(entries)):
                     #print entries[index]
                     if index not in self.ignore_cols:
                         entry = entries[index].strip()
                         if entry:
-                            self.csv_df[index].append(entry)
+                            row.append(entry)
+                        else:
+                            print "There are null element in row %s" % l_index
+                self.csv_df.append(row)
             else:
-                self.runable = False
-                print "Not consistent in term of number of columns and the length of the row"
+                print "Not consistent in term of number of columns and the length of the row at row %s" % l_index
 
     def readfile(self, filename):
         with open(filename, "rb") as f2r:
             header_l = f2r.readline()
             self.header_f = True
-            self.clean(header_l)
+            self.clean(header_l, 0)
             self.noc = len(self.header)
             self.header_f = False
-            ignore_index = 0
+            print self.header[2][0]
             for i in range(self.noc):
-                if self.header[i] == "?:":
-                    self.ignore_cols.append(ignore_index)
-                ignore_index += 1
-            self.csv_df = [[] for _ in xrange(self.noc - len(self.ignore_cols))]
-            while self.runable:
+                if self.header[i][0] == "?":
+                    self.ignore_cols.append(i)
+            r_index = 0
+            row = f2r.readline()
+            while row:
+                self.clean(row, r_index)
+                r_index += 1
                 row = f2r.readline()
-                if row:
-                    self.clean(row)
-                else:
-                    self.runable = False
+
         return self.csv_df
 
 
 readcsv = readCSV()
-df = readcsv.readfile("./POM3A.csv")
+start = time.time()
+df = readcsv.readfile("./example.csv")
+end =  time.time()
+print "Time that take to read csv table: %f" % (end - start)
+print df
 print len(df)
-
-
