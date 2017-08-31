8520032b150d433c11d5fe5fc7ca5961c3556dd9
Ken (Huy) Tu <huyqtu7@gmail.com>
2017-08-31 12:53:20 -0400

    Add files via upload

--- hw1/readcsv.py
+++ hw1/readcsv.py
@@ -7,6 +7,7 @@ class readCSV():
         self.noc = -1
         self.header_f = False
         self.ignore_cols = []
+        self.dtype = []
 
     def format(val):
         try:
@@ -18,11 +19,14 @@ class readCSV():
         except ValueError:
             return val
 
-    def clean(self, line, l_index):
+    def remove_mis(self, line):
         if "#" in line:
             line = line.split("#")[0]
         line = line.strip('\n')
         line = "".join(line.split())
+        return line
+
+    def clean(self, line, l_index):
         entries = line.split(",")
         #print entries
         if self.header_f:
@@ -46,22 +50,30 @@ class readCSV():
                             print "There are null element in row %s" % l_index
                 self.csv_df.append(row)
             else:
+                print entries
                 print "Not consistent in term of number of columns and the length of the row at row %s" % l_index
 
     def readfile(self, filename):
         with open(filename, "rb") as f2r:
             header_l = f2r.readline()
             self.header_f = True
+            header_l = self.remove_mis(header_l)
             self.clean(header_l, 0)
             self.noc = len(self.header)
             self.header_f = False
             for i in range(self.noc):
                 if self.header[i][0] == "?":
                     self.ignore_cols.append(i)
-            r_index = 0
+            r_index = 2
             row = f2r.readline()
             while row:
-                self.clean(row, r_index)
+                row = self.remove_mis(row)
+                if not row.endswith(','):
+                    self.clean(row, r_index)
+                else:
+                    print "The line %s is incomplete, concatenate to the next line" % r_index
+                    row += self.remove_mis(f2r.readline())
+                    self.clean(row, r_index)
                 r_index += 1
                 row = f2r.readline()
 
@@ -70,8 +82,10 @@ class readCSV():
 
 readcsv = readCSV()
 start = time.time()
-df = readcsv.readfile("./example.csv")
-end =  time.time()
+df = readcsv.readfile("./POM3A.csv")
+end = time.time()
 print "Time that take to read csv table: %f" % (end - start)
-print df
+#print df
 print len(df)
+
+
