e55cc3e01c380c6ff369b1ba0b768b7518571bdd
HuyTu7 <huyqtu7@gmail.com>
2017-09-12 04:39:41 -0400

    Updated the source code a bit

--- hw2/readData.py
+++ hw2/readData.py
@@ -133,16 +133,16 @@ start = time.time()
 filename = sys.argv[-1]
 sorted_dom_scores = readData.read_table(filename)
 end = time.time()
+print "There are %s entries in the table" % len(sorted_dom_scores)
 print "Time that takes to read csv table: %f" % (end - start)
 print readData.header.header
 print "Top 5 rows for their domination scores"
 for top in sorted_dom_scores[-5:]:
     print top.cells, top.id
-print "Bottom 5 rows for their domination scores"
+print "\nBottom 5 rows for their domination scores"
 for bottom in sorted_dom_scores[:5]:
     print bottom.cells, bottom.id
 
-print "There are %s entries in the table" % len(sorted_dom_scores)
 
 
 
