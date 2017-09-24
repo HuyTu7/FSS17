fdca8c37ae4a770da26e776fd5088c5d6f8e2f1c
HuyTu7 <huyqtu7@gmail.com>
2017-09-24 17:49:11 -0400

    updated the test file for unsupervised discretization

--- discretization/range_test.py
+++ discretization/range_test.py
@@ -11,14 +11,15 @@ def range_test():
     m = 0.7
     RANDOM = R()
     RANDOM.rseed(1)
-    tmp = dict()
+    tmp = []
     for i in range(1, 1001):
-        tmp[str(len(tmp)+1)] = RANDOM.r()**2
+        tmp.append((RANDOM.r()**2, len(tmp)+1))
     print("ranges: ")
-    print("%5s\t%5s\t%5s\t%5s\n", "#", "n", "lo", "hi")
-    print("%5s\t%5s\t%5s\t%5s\n", "-----", "-----", "-----", "-----")
-    for i, k in enumerate(RANGE.unsup_discret(tmp, f, 0)):
-        print("%5s\t%5s\t%5.3f\t%5.3f\n", i, k['n'], k['lo'], k['hi'])
+    print("%5s\t%5s\t%5s\t%5s\n" % ("#", "n", "lo", "hi"))
+    print("%5s\t%5s\t%5s\t%5s\n" % ("-----", "-----", "-----", "-----"))
+    ranges = RANGE.unsup_discret(tmp, f, 0, cohen, m)
+    for i, k in enumerate(ranges):
+        print("%5s\t%5s\t%5.3f\t%5.3f\n" % (i, k['n'], k['lo'], k['hi']))
 
 
 if __name__ == "__main__":
