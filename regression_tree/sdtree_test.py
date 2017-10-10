076b5e486bf3f97ab15c9197b08b57064890a971
HuyTu7 <huyqtu7@gmail.com>
2017-10-10 12:33:03 -0400

    updated for testing sdtree

---
+++ regression_tree/sdtree_test.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../utils/')
+import sdtree as TREE
+from table import TBL
+
+
+def sdTree(f, y):
+    TREE.tree_min_depth = 10
+    y = y or "dom"
+    f = f or "auto.csv"
+
+    tb1 = TBL(f)
+    tb2 = tb1.discretize_rows(getattr(tb1, y))
+    print "finishing discretizing rows"
+    tr = TREE.grow(tb2, y=tb1.dom)
+    print "finishing growing tree"
+    TREE.tprint(tr, 0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python sdtree.py <csv_file>")
+        sys.exit(0)
+    else:
+        file = sys.argv[1]
+        sdTree(file, None)
