3fe10cf2bf03676cb9c0cd3d8dacdc92d2bdf38a
HuyTu7 <huyqtu7@gmail.com>
2017-10-10 12:32:47 -0400

    refractoring the code

--- regression_tree/sdtree.py
+++ regression_tree/sdtree.py
@@ -1,15 +1,12 @@
 import sys
 sys.path.append('../utils/')
-sys.path.append('../hw2/')
 from num import NUM
-from table import TBL
-num = NUM()
 tree_min_depth = 2
 tree_max_depth = 10
 
-
 class tree():
     def __init__(self, t, yfun, pos, attr, val):
+        num = NUM()
         self._t = t
         self._kids = []
         self.yfun = yfun
@@ -27,25 +24,25 @@ def create(t, yfun, pos, attr, val):
 def order(t, y):
     def xpect(col):
         tmp = 0
-        for x in col["nums"]:
-            tmp += x.sd * x.n/float(col["n"])
+        for _, k in col["nums"].items():
+            tmp += (k.sd * k.n/float(col["n"]))
         return tmp
 
     def whatif(head, y):
-        col = {"pos": head.pos, "what": head.txt, "nums": {}, "n": 0.0}
+        col = {"pos": head.pos, "what": head.txt, "nums": {}, "n": 0}
         for r in t.rows:
             x = r.cells[col["pos"]]
             if x != '?':
                 col["n"] += 1
-                if x not in col["nums"]:
-                    col["nums"][x] = num
-                col["nums"][x].update(float(y(r)))
+                if x not in col["nums"].keys():
+                    col["nums"][x] = NUM()
+                col["nums"][x].update(y(r))
         return {"key": xpect(col), "val": col}
     out = []
     for h in t.x["cols"]:
         out.append(whatif(h, y))
-    out = sorted(out, key=lambda x, y: x["key"] < y["key"])
-    order_out = [x["val"] for x in out]
+    out = sorted(out, key=lambda x: x["key"])
+    order_out = [o["val"] for o in out]
     return order_out
 
 
@@ -69,7 +66,7 @@ def grow1(above, yfun, rows, lvl, b4, pos, attr, val):
                         rows1 = kids[val] if val in kids.keys() else []
                         rows1.append(r)
                         kids[val] = rows1
-                for val in kids:
+                for val in kids.keys():
                     rows1 = kids[val]
                     if len(rows1) < len(rows):
                         grow1(here, yfun, rows1, lvl+1, here.stats.sd, cut["pos"], cut["what"], val)
@@ -89,44 +86,21 @@ def tprint(tr, lvl):
 
     lvl = lvl or 0
     suffix = ""
-    if len(tr["_kids"]) == 0 or lvl == 0:
-        suffix = "n=%s mu=%-.2f sd=%-.2f"%(tr.stats.n, tr.stats.mean, tr.stats.sd)
+    if len(tr._kids) == 0 or lvl == 0:
+        suffix = "n=%s mu=%-.2f sd=%-.2f" % (tr.stats.n, tr.stats.mu, tr.stats.sd)
 
     if lvl == 0:
-        print "\n" + suffix
+        print("\n" + suffix)
     else:
-        print left(pad() + (str(tr.attr) or "") + " = " + (str(tr.val) or "")) + "\t\t\t:" + suffix
+        print(left(pad() + (str(tr.attr) or "") + " = " + (str(tr.val) or "")) + "\t\t\t:" + suffix)
     for j in range(len(tr._kids)):
-        tprint(tr._kids[j], lvl +1)
+        tprint(tr._kids[j], lvl+1)
 
 
 def leaf(tr, cells, bins, lvl):
     lvl = lvl or 0
-    for j, kid in enumerate(tr._kids):
-        pos, val = kid.pos, kid.val
-        if cells[pos] == val:
+    for _, kid in tr._kids.items():
+        if cells[kid.pos] == kid.val:
             return leaf(kid, cells, bins, lvl+1)
     return tr
 
-
-def sdTree(f, y):
-    global tree_min_depth
-    tree_min_depth = 10
-    y = y or "dom"
-    f = f or "auto.csv"
-
-    tb1 = TBL(f)
-    tb2 = tb1.discretize_rows(getattr(tb1, y))
-    print "finishing discretizing rows"
-    tr = grow(tb2, y=tb1.dom)
-    print "finishing growing tree"
-    tprint(tr)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python sdtree.py <csv_file>")
-        sys.exit(0)
-    else:
-        file = sys.argv[1]
-        sdTree(file, None)
