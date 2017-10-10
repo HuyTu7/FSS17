2902c3ac2e882848fb513782f19e2a5fe6a3c59b
HuyTu7 <huyqtu7@gmail.com>
2017-10-10 14:21:30 -0400

    updated for merging

--- discretization/suprange_test.py
+++ discretization/suprange_test.py
@@ -1,16 +1,13 @@
 import sys
-sys.path.append('../utils/')
-from sample_random import Random as RANDOM
 import range_1 as RANGE
 import super_range as SUPER_R
+sys.path.append('../utils/')
+from sample_random import Random as RANDOM
 from num import NUM
 
 
 def x(z): return z[0]
-
-
-def y(z):
-    return z[-1]
+def y(z): return z[-1]
 
 
 def klass(z):
@@ -23,13 +20,13 @@ def klass(z):
 def test():
     t, n = [], NUM()
     R = RANDOM()
-    split = [0.5, 0.7]
-    for i in range(1, 101):
+    split = [0.2, 0.5]
+    for i in range(1, 51):
         w = R.r()
         n.update(klass(w))
         t.append((w, klass(w)))
     print("\nWe have many unsupervised ranges.")
-    for j, one in enumerate(RANGE.unsup_discret(t, x, 0, split[0], split[1])):
+    for j, one in enumerate(RANGE.unsup_discret(t, x, 0, split)):
         print("x", j, one)
     print("\nWe have fewer supervised ranges.")
     for j, one in enumerate(SUPER_R.sup_discretize(t, x, y, True, True, split)):
