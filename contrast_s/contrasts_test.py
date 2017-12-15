e5184ed0997df4c46de7f544993b4e727cb78aaa
HuyTu7 <huyqtu7@gmail.com>
2017-12-15 16:36:53 -0500

    updated the test to out put the file

--- contrast_s/contrasts_test.py
+++ contrast_s/contrasts_test.py
@@ -3,10 +3,18 @@ sys.path.append('../regression_tree/')
 from sdtree_test import sdTree
 from contrast_sets import branches, plans, monitors
 
+
+def record(data, fname):
+    with open("%s_result.txt" % fname, "w") as text_file:
+        text_file.write(data)
+
+
 if __name__ == '__main__':
     file = sys.argv[1]
     built_tree = sdTree(file, None, 8)
     branches = branches(built_tree)
 
-    plans(branches)
-    monitors(branches)
+    _, plans_data = plans(branches)
+    record(plans_data, "plans")
+    _, monitors_data = monitors(branches)
+    record(monitors_data, "monitors")
